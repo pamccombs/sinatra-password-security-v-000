d632cce9ed2aac3c800deb18291cf755cb185156
Patrick McCombs <pa.mccombs1@gmail.com>
2019-04-25 18:46:13 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,6 +39,8 @@ class ApplicationController < Sinatra::Base
 		user = User.find_by(:username => params[:username])
 		
 		if user
+		  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
       redirect "/success"
     else
       redirect "/failure"
